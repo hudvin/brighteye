b7ea5316e5b052f349def9b8fa910473ff622a14
hudvin <hudvin@gmail.com>
2017-04-09 22:40:33 +0300

    args - face dims, photos per person, num of persons

--- facenet_experiments/utils/create_dataset.py
+++ facenet_experiments/utils/create_dataset.py
@@ -1,37 +1,67 @@
 import argparse
 import glob
+import os
 import sys
+from distutils.dir_util import mkpath
+
+import shutil
+
+import cv2
+from face_analyzer import FaceDetector
 
 
 def main(args):
-    persons_dirs = glob.glob(args.input_dataset_dir + "/*")
-    persons_files = map(lambda dir: (dir, glob.glob(dir + "/*")), persons_dirs)
+    print(args)
+    min_face_widht, min_face_height = map(lambda x: int(x), args.min_face_dims.split("x"))
+    persons_dirs = glob.glob(args.input_dir + "/*")
+    persons_files = map(lambda dir: (dir,
+                                     filter(lambda filepath: filepath.endswith((".jpg", ".png", ".jpeg")),
+                                            glob.glob(dir + "/*"))),
+                        persons_dirs)
+    if args.max_persons != -1:
+        persons_files = persons_files[:args.max_persons]
 
+    face_detector = FaceDetector()
     person_counter = 0
     for person_dir, person_files in persons_files:
         person_counter += 1
         file_counter = 0
-        for persons_file in persons_files:
-            #get number of faces
-            #align face
-            #get eye distance
-            print persons_file
-            file_counter += 1
-            if file_counter >= args.photos_per_person:
-                break
+        output_person_dir = args.output_dir + "/" + os.path.basename(person_dir)
+        mkpath(output_person_dir)
+        for person_file in person_files:
+            print person_file
+            faces = face_detector.find_faces(cv2.imread(person_file))
+            if not faces:
+                print "skipping %s, no faces found" % (person_file)
+            elif len(faces) > 1:
+                print "skipping %s, contains %s faces" % (person_file, len(faces))
+            else:
+                face = faces[0]
+                face_width = face["width"]
+                face_height = face["height"]
+                if face_width >= min_face_widht and face_height >= min_face_height:
+                    file_counter += 1
+                    shutil.copyfile(person_file, output_person_dir + "/" + os.path.basename(person_file))
+                    if args.max_photos != -1 and file_counter >= args.max_photos:
+                        break
+                else:
+                    print "skipping %s, because face dims are too small - (%s x %s)" % (
+                    person_file, face_width, face_height)
         print("person counter: %s" % person_counter)
+
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument('input_dataset_dir', type=str,
-        help='Directory containing dirs with photos of persons ')
-    #parser.add_argument('output_dataset_dir', type=str,
-    #                    help='Directory to store processed images grouped by person name')
-    #parser.add_argument('min_eye_distance', type=int, help='Min distance between eyes')
-    #parser.add_argument('max_eye_distance', type=int, help='Min distance between eyes')
-    parser.add_argument('photos_per_person', type=int, help='Number of photos per each person')
-
+    parser.add_argument('--input-dir', type=str,
+                        help='Directory containing dirs with photos of persons ')
+    parser.add_argument('--output-dir', type=str,
+                        help='Directory to store processed images grouped by person name')
+    parser.add_argument('--max-photos', type=int, help='Number of photos per each person', default=-1)
+    parser.add_argument('--max-persons', type=int, help='Max number of persons', default=-1)
+    parser.add_argument('--min_face_dims', type=str, help='Min dims(widthxheight) of face', default="100x100")
+    # parser.add_argument('--num-faces', type=int, help='Max number of faces on image', default=1)
     return parser.parse_args(argv)
+
 
 if __name__ == '__main__':
     main(parse_arguments(sys.argv[1:]))
--- facenet_experiments/utils/face_analyzer.py
+++ facenet_experiments/utils/face_analyzer.py
@@ -1,4 +1,33 @@
 import cv2
+import dlib
+
+
+class DlibLoader:
+    def __init__(self):
+        pass
+
+
+class FaceAnalyzer:
+    def __init__(self):
+        pass
+
+
+class FaceDetector:
+
+    def __init__(self):
+        self.face_detector = dlib.get_frontal_face_detector()
+
+    def find_faces(self, image_bytes):
+        detection_results = self.face_detector(image_bytes, 1)
+        faces = []
+        for i, face_rect in enumerate(detection_results):
+            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+                i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
+            faces.append({
+                "top_left": (face_rect.left(), face_rect.top()),
+                "bottom_right": (face_rect.right(), face_rect.bottom()),
+                "width": face_rect.right()-face_rect.left(), "height":  face_rect.bottom()-face_rect.top()})
+        return faces
 
 #
 # class FaceAnalyzer:
@@ -24,4 +53,3 @@ import cv2
 #     rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
 #     bbs = aligner.getAllFaceBoundingBoxes(rgb_img)
 #     return len(bbs)
-
