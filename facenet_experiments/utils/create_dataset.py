7c227fa29e35b6214313f8b5b21b176b226426f9
hudvin <hudvin@gmail.com>
2017-04-18 20:02:49 +0300

    fix exception if no images at all are found

--- facenet_experiments/utils/create_dataset.py
+++ facenet_experiments/utils/create_dataset.py
@@ -1,6 +1,7 @@
 import argparse
 import glob
 import sys
+import traceback
 
 from functools import partial
 
@@ -28,39 +29,42 @@ def main(args):
 
 
 def process_persons(args, output_dirs, person_files):
-    min_face_widht, min_face_height = map(lambda x: int(x), args.min_face_dims.split("x"))
-    (dir, files) = person_files
-    if not output_dirs.is_embeddings_exist(dir):
-        face_detector = FaceDetector()
-        face_filter = FaceFilter(face_detector)
-        centroid_filter = CentroidFilter()
-        single_face_images = []
-        for person_file in files:
-            print person_file
-            error, result = face_filter.filter(person_file, min_face_widht, min_face_height)
-            if not error:
-                single_face_images.append(person_file)
-            else:
-                print result
-                if error == Errors.MANY_FACES:
-                    output_dirs.copy_to_multi_faces(person_file)
-                    pass
-                elif error == Errors.SMALL_FACE:
-                    output_dirs.copy_to_small_faces(person_file)
-                elif error == Errors.NO_FACES:
-                    output_dirs.copy_to_no_faces(person_file)
-        print "generating embeddings, apply centroid filtering"
-        bad, good = centroid_filter.filter(single_face_images, 0.5)
+    try:
+        min_face_widht, min_face_height = map(lambda x: int(x), args.min_face_dims.split("x"))
+        (dir, files) = person_files
+        if not output_dirs.is_embeddings_exist(dir):
+            face_detector = FaceDetector()
+            face_filter = FaceFilter(face_detector)
+            centroid_filter = CentroidFilter()
+            single_face_images = []
+            for person_file in files:
+                print person_file
+                error, result = face_filter.filter(person_file, min_face_widht, min_face_height)
+                if not error:
+                    single_face_images.append(person_file)
+                else:
+                    print result
+                    if error == Errors.MANY_FACES:
+                        output_dirs.copy_to_multi_faces(person_file)
+                        pass
+                    elif error == Errors.SMALL_FACE:
+                        output_dirs.copy_to_small_faces(person_file)
+                    elif error == Errors.NO_FACES:
+                        output_dirs.copy_to_no_faces(person_file)
+            print "generating embeddings, apply centroid filtering"
+            if single_face_images:
+                bad, good = centroid_filter.filter(single_face_images, 0.5)
 
-        for record in bad:
-            output_dirs.copy_to_outlier_faces(record[0])
+                for record in bad:
+                    output_dirs.copy_to_outlier_faces(record[0])
 
-        max = args.max_photos if args.max_photos != -1 else len(good)
-        good = good[:max]
-        for record in good:
-            output_dirs.copy_to_cleaned(record[0])
-        output_dirs.save_embeddings(dir, good)
-
+                max = args.max_photos if args.max_photos != -1 else len(good)
+                good = good[:max]
+                for record in good:
+                    output_dirs.copy_to_cleaned(record[0])
+                output_dirs.save_embeddings(dir, good)
+    except Exception:
+        print(traceback.format_exc())
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
@@ -71,7 +75,7 @@ def parse_arguments(argv):
     parser.add_argument('--max-photos', type=int, help='Number of photos per each person', default=-1)
     parser.add_argument('--max-persons', type=int, help='Max number of persons', default=-1)
     parser.add_argument('--min-face-dims', type=str, help='Min dims(widthxheight) of face', default="100x100")
-    parser.add_argument('--threads', type=int, help='Num of threads to use', default=5)
+    parser.add_argument('--threads', type=int, help='Num of threads to use', default=6)
     return parser.parse_args(argv)
 
 
